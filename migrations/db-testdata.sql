0a5add45410a22593a6e4f64c4cd85e1228e05e8
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-12-31 20:13:51 +0530

    Started new admin.

--- migrations/db-testdata.sql
+++ migrations/db-testdata.sql
@@ -8,12 +8,12 @@ SELECT @site := `id` FROM cmg_core_site WHERE slug = 'main';
 -- Twitter Meta Config Form
 --
 
-INSERT INTO `cmg_core_form` (`siteId`,`templateId`,`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`successMessage`,`captcha`,`visibility`,`active`,`userMail`,`adminMail`,`options`,`createdAt`,`modifiedAt`) VALUES
+INSERT INTO `cmg_core_form` (`siteId`,`templateId`,`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`successMessage`,`captcha`,`visibility`,`active`,`userMail`,`adminMail`,`htmlOptions`,`createdAt`,`modifiedAt`) VALUES
 	(@site,NULL,1,1,'Config Twitter Meta','config-twitter-meta','system','Twitter meta configuration form.','All configurations saved successfully.',0,10,1,0,0,NULL,'2014-10-11 14:22:54','2014-10-11 14:22:54');
 
 SELECT @form := `id` FROM cmg_core_form WHERE slug = 'config-twitter-meta';
 
-INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`validators`,`options`,`data`,`order`) VALUES 
+INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`validators`,`htmlOptions`,`data`,`order`) VALUES 
 	(@form,'active','Active',20,0,'required','{\"title\":\"activate or de-activate.\"}',NULL,0),
 	(@form,'page','Page',20,0,'required','{\"title\":\"enable or disabled for all pages.\"}',NULL,0),
 	(@form,'post','Post',20,0,'required','{\"title\":\"enable or disabled for all posts.\"}',NULL,0),
@@ -25,12 +25,12 @@ INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`va
 -- Facebook Meta Config Form
 --
 
-INSERT INTO `cmg_core_form` (`siteId`,`templateId`,`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`successMessage`,`captcha`,`visibility`,`active`,`userMail`,`adminMail`,`options`,`createdAt`,`modifiedAt`) VALUES
+INSERT INTO `cmg_core_form` (`siteId`,`templateId`,`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`successMessage`,`captcha`,`visibility`,`active`,`userMail`,`adminMail`,`htmlOptions`,`createdAt`,`modifiedAt`) VALUES
 	(@site,NULL,1,1,'Config Facebook Meta','config-facebook-meta','system','Facebook meta configuration form.','All configurations saved successfully.',0,10,1,0,0,NULL,'2014-10-11 14:22:54','2014-10-11 14:22:54');
 
 SELECT @form := `id` FROM cmg_core_form WHERE slug = 'config-facebook-meta';
 
-INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`validators`,`options`,`data`,`order`) VALUES 
+INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`validators`,`htmlOptions`,`data`,`order`) VALUES 
 	(@form,'active','Active',20,0,'required','{\"title\":\"activate or de-activate.\"}',NULL,0),
 	(@form,'page','Page',20,0,'required','{\"title\":\"enable or disabled for all pages.\"}',NULL,0),
 	(@form,'post','Post',20,0,'required','{\"title\":\"enable or disabled for all posts.\"}',NULL,0),
@@ -42,16 +42,16 @@ INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`va
 -- Dumping data for table `cmg_core_model_attribute`
 --
 
-INSERT INTO `cmg_core_model_attribute` (`parentId`,`parentType`,`name`,`value`,`type`) VALUES
-	(@site,'site','active','1','twitter-meta'),
-	(@site,'site','page','1','twitter-meta'),
-	(@site,'site','post','1','twitter-meta'),
-	(@site,'site','card','summary_large_image','twitter-meta'),
-	(@site,'site','site',NULL,'twitter-meta'),
-	(@site,'site','creator',NULL,'twitter-meta'),
-	(@site,'site','active','1','facebook-meta'),
-	(@site,'site','page','1','facebook-meta'),
-	(@site,'site','post','1','facebook-meta'),
-	(@site,'site','app_id',NULL,'facebook-meta'),
-	(@site,'site','author',NULL,'facebook-meta'),
-	(@site,'site','publisher',NULL,'facebook-meta');
+INSERT INTO `cmg_core_model_attribute` (`parentId`,`parentType`,`name`,`type`,`valueType`,`value`) VALUES
+	(@site,'site','active','twitter-meta','flag','1'),
+	(@site,'site','page','twitter-meta','flag','1'),
+	(@site,'site','post','twitter-meta','flag','1'),
+	(@site,'site','card','twitter-meta','text','summary_large_image'),
+	(@site,'site','site','twitter-meta','text',NULL),
+	(@site,'site','creator','twitter-meta','text',NULL),
+	(@site,'site','active','facebook-meta','flag','1'),
+	(@site,'site','page','facebook-meta','flag','1'),
+	(@site,'site','post','facebook-meta','flag','1'),
+	(@site,'site','app_id','facebook-meta','text',NULL),
+	(@site,'site','author','facebook-meta','text',NULL),
+	(@site,'site','publisher','facebook-meta','text',NULL);
